651283c99687dab26df3ea0450065e2f5288825e
baharriyan <indonesiariyan@gmail.com>
2020-10-20 19:01:43 +0700

    first commit

---
+++ PBOIF2_10116416_LATIHAN10_Logika/src/PBOIF2_10116416_Latihan10Logika.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Riyan Bahar
+ * Nama  : Riyan Bahar
+ * Kelas : IF-2
+ * Nim   : 10116416
+ * 
+ */
+public class PBOIF2_10116416_Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+    }
+    
+}
